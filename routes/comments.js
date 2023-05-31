f76673d8ef5ebe6484a583e5862b4edc2a8e2945
Yeuda Shtern <yeudashtern@gmail.com>
2023-05-31 16:32:42 +0300

    jhbgj

--- routes/comments.js
+++ routes/comments.js
@@ -105,4 +105,17 @@ router.delete("/:id", auth, async (req, res) => {
 })
 
 
+router.get("/count", async(req,res) => {
+    let perPage = Math.min(req.query.perPage, 20) || 5;
+    try{
+      let data = await CommentsModel.countDocuments(perPage);
+      res.json({count:data,pages:Math.ceil(data/perPage)})
+    }
+    catch(err){
+      console.log(err);
+      res.status(502).json({err})
+    }
+  })
+
+
 module.exports = router;
